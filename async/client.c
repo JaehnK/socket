841b964b1972fcd1abb85e6ed201f124958a1eb5
Jaehun Kim <jaehukim@c3r4s6.42gyeongsan.kr>
2025-05-06 15:33:38 +0900

    [Feat] Async Finish

--- async/client.c
+++ async/client.c
@@ -10,55 +10,166 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-int main(void)
+#define MAX_EVENTS 10
+
+int     open_socket()
 {
-    int                 socket_fd;
-    struct sockaddr_in *socket_addr;
-    char                buf[256];
-    int                 epfd, nfds;
-    struct epoll_event  ev, events[10];
+    int socket_fd;
 
     socket_fd = socket(PF_INET, SOCK_STREAM, 0);
-    if (socket_fd < 0)
+    if (socket_fd == -1)
     {
-        write(1, "socket fd\n", 11);
+        perror("socket");
         return (-1);
     }
-    
-    int flags = fcntl(socket_fd, F_GETFL, 0);
-    fcntl(socket_fd, F_SETFL, flags| O_NONBLOCK);
+    return (socket_fd);
+}
 
-    socket_addr = calloc(sizeof(struct sockaddr_in), 1);
+int     init_async(int socket_fd, struct sockaddr_in *socket_addr)
+{
+    int flag;
+    int result;
+
+    flag = fcntl(socket_fd, F_GETFL, 0);
+    fcntl(socket_fd, F_SETFL, flag | O_NONBLOCK);
+
     socket_addr->sin_family = AF_INET;
     socket_addr->sin_port = htons(8080);
-    
+
     if (inet_pton(AF_INET, "127.0.0.1", &socket_addr->sin_addr) <= 0)
     {
-        write(1, "inet_pton\n", 11);
+        perror("inet_pton");
         return (-1);
     }
-
-    int result = connect(socket_fd, (struct sockaddr *)socket_addr, sizeof(struct sockaddr_in));
+    
+    result = connect(socket_fd, (struct sockaddr *)socket_addr, sizeof(struct sockaddr_in));
     if (result < 0 && errno != EINPROGRESS)
     {
         perror("connect");
         return (-1);
     }
 
-    epfd = epoll_create1(0);
+    return (0);
+}
+
+int     init_epoll(int socket_fd, struct epoll_event *ev)
+{
+    int epfd;
+
+    epfd = epoll_create(1);
     if (epfd == -1)
     {
-        perror("epoll");
+        perror("epoll_create");
         return (-1);
     }
-    epoll_ctl(epfd, )
+    
+    ev->events = EPOLLOUT | EPOLLIN;
+    ev->data.fd = socket_fd;
+    if (epoll_ctl(epfd, EPOLL_CTL_ADD, socket_fd, ev) == -1)
+    {
+        perror("epoll_ctl: socket_fd");
+        return (-1);
+    }
 
-    printf("Waiting to send: ");
-    fgets(buf, sizeof(buf), stdin);
-    buf[strcspn(buf, "\n")] = '\0'; 
-    send(socket_fd, buf, strlen(buf), 0);
+    ev->events = EPOLLIN;
+    ev->data.fd = STDIN_FILENO;
+    if (epoll_ctl(epfd, EPOLL_CTL_ADD, STDIN_FILENO, ev) == -1)
+    {
+        perror("epoll_ctl: stdin");
+        return (-1);
+    }
 
-    free(socket_addr);
-    close(socket_fd);
+    return (epfd);
 }
-//https://claude.ai/chat/8852b4ac-f76f-4aea-9d16-f39096bc24ea
+
+void    free_close_exit(int socket_fd, int epfd, int flag)
+{
+    close(socket_fd);
+    close(epfd);
+    exit(flag);
+}
+
+int main(void)
+{
+    int                 socket_fd;
+    struct sockaddr_in socket_addr;
+    char                buf[256];
+    int                 epfd, nfds;
+    struct epoll_event  ev, events[10];
+    
+    int connected, sent, errflag;
+    socklen_t   size;
+
+    socket_fd = open_socket();
+    if (socket_fd == -1)
+        return (-1);
+    
+    if (init_async(socket_fd, &socket_addr) == -1)
+        return (-1);
+
+    if ((epfd = init_epoll(socket_fd, &ev)) == -1)
+        return (-1);
+
+    connected = 0;
+    sent = 0;
+    size = sizeof(errflag);
+    while (1)
+    {
+        nfds = epoll_wait(epfd, events, MAX_EVENTS, -1);
+        if (nfds == -1)
+        {
+            printf("epfd: %d\n", epfd);
+            perror("epoll_wait");
+            break ;
+        }
+        for (int i = 0; i < nfds; i++)
+        {
+            if (events[i].data.fd == socket_fd && \
+                (events[i].events & EPOLLOUT))
+            {
+                if (connected == 0)
+                {
+                    if (getsockopt(socket_fd, SOL_SOCKET, \
+                            SO_ERROR, &errflag, &size) == -1)
+                    {
+                        perror("getsockopt");
+                        free_close_exit(socket_fd, epfd, 1);
+                    }
+                    if (errflag != 0)
+                    {
+                        perror("Failed to Connect");
+                        free_close_exit(socket_fd, epfd, 1);
+                    }
+                    printf("Sucess to Connect. Input Message and enter\n");
+                    connected = 1;
+                }
+                else if (!sent && strlen(buf) > 0)
+                {
+                    send(socket_fd, buf, strlen(buf), 0);
+                    printf("Message Go: %s\n", buf);
+                    sent = 1;
+                    
+                    ev.events = EPOLLIN;
+                    ev.data.fd = socket_fd;
+                    epoll_ctl(epfd, EPOLL_CTL_MOD, socket_fd, &ev);
+                }
+            }
+            else if (events[i].data.fd == STDIN_FILENO)
+            {
+                memset(buf, 0, 256);
+                if (fgets(buf, 256, stdin) == NULL)
+                    free_close_exit(socket_fd, epfd, 1);
+                buf[strcspn(buf, "\n")] = '\0';
+                    
+                    
+                if (connected) 
+                {
+                    ev.events = EPOLLOUT;
+                    ev.data.fd = socket_fd;
+                    epoll_ctl(epfd, EPOLL_CTL_MOD, socket_fd, &ev);
+                    sent = 0;
+                }
+            }
+        }
+    }
+}
--- async/server.c
+++ async/server.c
@@ -1,66 +1,190 @@
-#include <sys/socket.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <unistd.h>
+#include <arpa/inet.h>
+#include <errno.h>
+#include <fcntl.h>
 #include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-#include <memory.h>
+#include <sys/epoll.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
 
+#define MAX_EVENTS 10
 
-
-
-int main()
+int open_socket()
 {
-    int                 socket_fd;
-    int                 accept_fd;
-    unsigned int        addr_size;
-    struct sockaddr_in  *socket_addr;
-    struct sockaddr_in  *client_addr;
-    char                buf[256];
-    
+    int socket_fd;
+
     socket_fd = socket(PF_INET, SOCK_STREAM, 0);
-    if (socket_fd < 0)
+    if (socket_fd == -1)
     {
-        write(1, "socket fd\n", 11);
+        perror("socket");
         return (-1);
     }
-    
-    printf("socket: %d\n", socket_fd);
-    
-    socket_addr = calloc(sizeof(struct sockaddr_in), 1);
+    printf("socket fd: %d\n", socket_fd);
+    return (socket_fd);
+}
+
+int init_nonblock_socket(int socket_fd)
+{
+    int flags, opt;
+
+    opt = 1;
+    if (setsockopt(socket_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) == -1)
+    {
+        perror("setsockopt");
+        return (-1);
+    }
+
+    flags = fcntl(socket_fd, F_GETFL, 0);
+    fcntl(socket_fd, F_SETFL, flags | O_NONBLOCK);
+
+    return (0);
+}
+
+int bind_and_listen(int socket_fd, struct sockaddr_in *socket_addr)
+{
+    memset(socket_addr, 0, sizeof(struct sockaddr_in));
     socket_addr->sin_family = AF_INET;
     socket_addr->sin_port = htons(8080);
     socket_addr->sin_addr.s_addr = htonl(INADDR_ANY);
-    
-    if (bind(socket_fd, (struct sockaddr *)socket_addr, sizeof(*socket_addr)) < 0)
+
+    if (bind(socket_fd, (struct sockaddr *)socket_addr, sizeof(*socket_addr)) == -1)
     {
-        write(1, "bind\n", 5);
-        return (-1);
-    }
-    
-    if (listen(socket_fd, 256) < 0)
-    {
-        write(1, "listen\n", 8);
+        perror("bind");
         return (-1);
     }
 
-    printf("Listening ...\n");
-
-    client_addr = calloc(sizeof(struct sockaddr_in), 1);
-    addr_size = sizeof(client_addr);
-    accept_fd = accept(socket_fd, (struct sockaddr *)client_addr, &addr_size);
-    if (accept_fd < 0)
+    if (listen(socket_fd, SOMAXCONN) == -1)
     {
-        write(1, "accept\n", 8);
+        perror("listen");
+        return (-1);
+    }
+    printf("listening ...\n");
+    return (0);
+}
+
+int init_epoll(int socket_fd, struct epoll_event *ev)
+{
+    int epfd;
+
+    epfd = epoll_create(1);
+    if (epfd == -1)
+    {
+        perror("epoll_create");
         return (-1);
     }
     
-    memset(buf, 0, sizeof(buf));
-    if (recv(accept_fd, buf, sizeof(buf) - 1, 0))
-        printf("Received: %s\n", buf);
+    ev->events = EPOLLIN;
+    ev->data.fd = socket_fd;
+    if (epoll_ctl(epfd, EPOLL_CTL_ADD, socket_fd, ev) == -1)
+    {
+        perror("epoll_ctl");
+        return(-1);
+    }
+
+    return (epfd);
+}
+
+int main()
+{
+    int                 socket_fd, epfd, nfds;
+    char                buf[256];
+    struct sockaddr_in  socket_addr;
+    struct epoll_event  ev, events[MAX_EVENTS];
     
-    close(accept_fd);
+    socket_fd = open_socket();
+    if (socket_fd == -1)
+        return (-1);
+    
+    if (init_nonblock_socket(socket_fd) == -1)
+        return (-1);
+    
+    if (bind_and_listen(socket_fd, &socket_addr) == -1)
+        return (-1);
+    
+    if ((epfd = init_epoll(socket_fd, &ev)) == -1)
+        return (-1);
+
+
+    struct sockaddr_in  client_addr;
+    socklen_t           addr_size;
+    int                 client_fd, flag;
+
+    while (1)
+    {
+        nfds = epoll_wait(epfd, events, MAX_EVENTS, -1);
+        if (nfds == -1)
+        {
+            perror("epoll_wait");
+            break ;
+        }
+
+        for (int i = 0; i < nfds; i++)
+        {
+            if (events[i].data.fd == socket_fd)
+            {
+                addr_size = sizeof(client_addr);
+                client_fd = accept(socket_fd, (struct sockaddr *) &client_addr, &addr_size);
+                if (client_fd == -1)
+                {
+                    if (errno == EAGAIN || errno == EWOULDBLOCK)
+                        continue ;
+                    else
+                    {
+                        perror("accept");
+                        continue ;
+                    }
+                }
+
+                flag = fcntl(client_fd, F_GETFL, 0);
+                fcntl(client_fd, F_SETFL, flag | O_NONBLOCK);
+
+                char client_ip[INET_ADDRSTRLEN];
+                inet_ntop(AF_INET, &(client_addr.sin_addr), client_ip, INET_ADDRSTRLEN);
+                printf("New Client Connect: %s %d (fd: %d)\n", \
+                        client_ip, ntohs(client_addr.sin_port), client_fd);
+
+                ev.events = EPOLLIN | EPOLLET;
+                ev.data.fd = client_fd;
+                if (epoll_ctl(epfd, EPOLL_CTL_ADD, client_fd, &ev) == -1)
+                {
+                    perror("epoll_ctl: client fd");
+                    close(client_fd);
+                    continue ;
+                }
+            }
+            else
+            {
+                int bytes;
+                client_fd = events[i].data.fd;
+                memset(buf, 0, 255);
+                bytes = recv(client_fd, buf, 255, 0);
+                if (bytes <= 0)
+                {
+                    if (bytes == 0) 
+                        printf("클라이언트 연결 종료 (fd: %d)\n", client_fd);
+                    else
+                        perror("recv");
+                    epoll_ctl(epfd, EPOLL_CTL_DEL, client_fd, NULL);
+                    close(client_fd);
+                    continue ;
+                }
+                printf("클라이언트 (fd: %d)로부터 수신: %s\n", client_fd, buf);
+                // 에코 서버로 작동하려면 다음 코드 추가
+                send(client_fd, buf, bytes, 0);
+                
+                // 응답 후 연결 유지하지 않고 바로 종료하려면
+                // printf("클라이언트 연결 종료 (fd: %d)\n", client_fd);
+                // epoll_ctl(epfd, EPOLL_CTL_DEL, client_fd, NULL);
+                // close(client_fd);
+            }
+
+        }
+    }
+  
+    close(epfd);
     close(socket_fd);
-
     return (0);
 }
